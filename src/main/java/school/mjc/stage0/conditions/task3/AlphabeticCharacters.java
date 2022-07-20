e4d97769025c7424634784d3b89f445ea3557227
tikmart <t.martirosyan012@gmail.com>
2022-07-20 22:34:17 +0400

    Task 3 second commit

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -1,17 +1,25 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
-    public void vowelDeterminer(char character) {
+    public static void vowelDeterminer(char character) {
+
         
-        character = 'o';
-        
-        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ){
+        if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ||
+                character == 'A' || character == 'E' || character == 'I' || character == 'O' || character == 'U' ){
 
             System.out.println("Vowel");
             
         } else if (character == 'b' || character == 'c' || character == 'd' || character == 'f'
-                || character == 'g' || character == 'h' || character == 'j' || character == 'k' || character == 'l' || character == 'm' || character == 'n' || character == 'p' || character == 'q' || character == 'r' || character == 's' || character == 't' ||
-                character == 'v' || character == 'x' || character == 'y' || character == 'z') {
+                || character == 'g' || character == 'h' || character == 'j' || character == 'k' || character == 'l' ||
+                character == 'm' || character == 'n' || character == 'p' || character == 'q' || character == 'r' ||
+                character == 's' || character == 't' ||
+                character == 'v' || character == 'x' || character == 'y' || character == 'z' ||
+
+                character == 'B' || character == 'C' || character == 'D' || character == 'F'
+                || character == 'G' || character == 'H' || character == 'J' || character == 'K' || character == 'L' ||
+                character == 'M' || character == 'N' || character == 'P' || character == 'Q' || character == 'R' ||
+                character == 'S' || character == 'T' ||
+                character == 'V' || character == 'X' || character == 'Y' || character == 'Z' ) {
             System.out.println("Consonant");
 
         } else {
@@ -20,4 +28,11 @@ public class AlphabeticCharacters {
 
 
     }
+
+    public static void main (String[] args){
+        vowelDeterminer('o');
+        vowelDeterminer('B');
+        vowelDeterminer('ł');
+    }
+
 }
